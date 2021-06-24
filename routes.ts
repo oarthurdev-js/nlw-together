9db7f56acc93c7190b1bac20239a0794ac07edfe
Arthur <arthur.lima@soulasalle.com.br>
2021-06-24 20:29:24 -0300

    dia 4, trabalhando com JWT

--- routes.ts
+++ routes.ts
@@ -3,18 +3,21 @@ import { CreateUserController } from './src/controllers/CreateUserController'
 import { CreateTagController } from './src/controllers/CreateTagController'
 import { ensureAdmin } from './src/middlewares/ensureAdmin'
 import { AuthenticateUserController } from "./src/controllers/AuthenticateUserController"
+import { CreateComplimentController } from "./src/controllers/CreateComplementController"
 
 const router = Router()
 
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const autheticateUserController = new AuthenticateUserController()
+const createComplimentController = new CreateComplimentController()
 
 
 
 router.post("/tags", ensureAdmin, createTagController.handle)
 router.post("/users", (createUserController.handle))
 router.post("/login", autheticateUserController.handle)
+router.post("/compliments", createComplimentController.handle)
 
 
 export { router }
